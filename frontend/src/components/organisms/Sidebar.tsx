1fb0523be4f568f76062c222a97b49da1c0ab1e8
Igor Soares <49894949+rogigs@users.noreply.github.com>
2025-01-14 17:53:02 -0300

    refactor: organisms Sidebar more accessible

--- frontend/src/components/organisms/Sidebar.tsx
+++ frontend/src/components/organisms/Sidebar.tsx
@@ -56,19 +56,18 @@ export const Sidebar = ({ children }: Children) => {
     <div className="relative min-h-screen">
       <button
         onClick={toggleSidebar}
-        aria-label="Toggle sidebar"
+        aria-label="Alternar barra lateral"
         className="fixed top-4 left-4 z-50 h-8 w-8 p-2 bg-gray-800 text-white rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
       >
         {isOpen ? <XMarkIcon /> : <Bars3Icon />}
       </button>
 
       <aside
-        className={`fixed top-0 left-0 z-40 min-h-screen h-screen transition-transform duration-300 ease-in-out transform ${
-          isOpen ? "translate-x-0 w-full md:w-64" : "-translate-x-full w-64"
-        } bg-gray-100 text-gray-800 shadow-lg`}
+        className={`fixed top-0 left-0 z-40 min-h-screen h-screen transition-transform duration-300 ease-in-out transform ${isOpen ? "translate-x-0 w-full md:w-64" : "-translate-x-full w-64"
+          } bg-gray-100 text-gray-800 shadow-lg`}
       >
         <nav className="h-full flex flex-col">
-          <div className="py-16 px-4 w-fit">
+          <div role="banner" className="py-16 px-4 w-fit">
             <Logo />
           </div>
           <ul className="space-y-4">
@@ -76,12 +75,13 @@ export const Sidebar = ({ children }: Children) => {
               <li key={item.title} className="hover:bg-gray-200 ">
                 <a
                   href="#"
-                  className="flex px-4  items-center space-x-3 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400 transition-colors duration-200"
-                  aria-label={item.title}
+                  className="flex px-4 items-center space-x-3 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400 transition-colors duration-200"
+                  aria-label={`Navegue para a página ${item.title} para exibir dados, inserir dados via formuláario ou via arquivo`}
                 >
-                  <span className="h-6 w-6">{item.icon}</span>
-                  <span className="text-lg">{item.title}</span>
+                  <span className="h-6 w-6" role="none">{item.icon}</span>
+                  <span className="text-lg" role="none">{item.title}</span>  
                 </a>
+
               </li>
             ))}
           </ul>
@@ -89,9 +89,8 @@ export const Sidebar = ({ children }: Children) => {
       </aside>
 
       <main
-        className={`p-4 ${
-          isOpen ? "md:ml-64" : ""
-        } transition-margin duration-300 ease-in-out`}
+        className={`p-4 ${isOpen ? "md:ml-64" : ""
+          } transition-margin duration-300 ease-in-out`}
       >
         <div className="pt-8 md:p-8 mt-4 md:ml-4">{children}</div>
       </main>
