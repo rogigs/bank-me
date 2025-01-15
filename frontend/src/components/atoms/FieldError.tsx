bcfcf8f5e24fa364bfd7ace432468ec20a5694f1
Igor Soares <igor.soares.developer@gmail.com>
2025-01-15 03:39:05 -0300

    fix: add red with good contrast

--- frontend/src/components/atoms/FieldError.tsx
+++ frontend/src/components/atoms/FieldError.tsx
@@ -1,14 +1,14 @@
 import { HTMLAttributes } from "react";
 
 type FormFieldErrorProps = HTMLAttributes<HTMLSpanElement> & {
-  message: string | undefined;
+  message?: string;
 };
 
 export const FormFieldError = ({ message, ...props }: FormFieldErrorProps) => {
   if (!message) return null;
 
   return (
-    <span role="alert" className="text-red-500" {...props}>
+    <span role="alert" className="text-red-600" {...props}>
       {message}
     </span>
   );
